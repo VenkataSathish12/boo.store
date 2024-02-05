65a3e9dcfd947138b75e81d20dfc9df5eb10a3e7
sathish <sanduvenkatasathish@sathish.com>
2024-02-05 12:58:24 +0530

    Created Web application book store

---
+++ WebApp.Bookstore/WebApp.Bookstore/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Bookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "WebApplication Book Store";
+        }
+    }
+}
